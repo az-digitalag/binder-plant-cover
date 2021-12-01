5e8758ee0db53bd35f6bd119656d5919840bca1f
Jessica Guo <jessicaguo@email.arizona.edu>
2021-12-01 15:58:09 -0700

    Updated to zero-inflated beta with flat rho

--- models/cover-invasive/Invasive_cover.R
+++ models/cover-invasive/Invasive_cover.R
@@ -22,19 +22,19 @@ dat <- read.csv("data/cover.csv")
 X <- model.matrix( ~ grazing * fuelbreak, data = dat) 
 colnames(X)
 
-# split the data into discrete and continuous components
-y.temp <- with(dat, ifelse(invasive_grass == 1 | invasive_grass == 0, 
+# split the data into 0 and continuous components
+y.temp <- with(dat, ifelse(invasive_grass == 0, 
                            invasive_grass, NA))
-y.discrete <- ifelse(is.na(y.temp), 0, 1)
+y.0 <- ifelse(is.na(y.temp), 0, 1)
 
 # group discrete response + predictors
 y.d <- y.temp[!is.na(y.temp)]
-x.d <- X[y.discrete == 1,]
-n.discrete <- length(y.d)
-block.d <- as.numeric(dat$block)[y.discrete == 1]
+x.d <- X[y.0 == 1,]
+n.0 <- length(y.d)
+# block.d <- as.numeric(dat$block)[y.0 == 1]
 
 # group continuous response + predictors
-which.cont <- which(y.discrete == 0)
+which.cont <- which(y.0 == 0)
 y.c <- dat$invasive_grass[which.cont]
 x.c <- X[which.cont,]
 n.cont <- length(y.c)
@@ -42,18 +42,7 @@ block.c <- as.numeric(dat$block)[which.cont]
 
 # Assemble model inputs
 datlist <- list(N = nrow(dat),
-                y.discrete = y.discrete,
-                n.discrete = n.discrete,
-                y.d = y.d, 
-                fall = x.d[,2],
-                spring = x.d[,3],
-                herbicide = x.d[,4],
-                greenstrip = x.d[,5],
-                fall_herbicide = x.d[,6],
-                spring_herbicide = x.d[,7],
-                fall_greenstrip = x.d[,8],
-                spring_greenstrip = x.d[,9],
-                block.d = block.d,
+                y.0 = y.0,
                 n.cont = n.cont,
                 y.c = y.c,
                 fall2 = x.c[,2],
@@ -91,7 +80,7 @@ initslist <- list(append(saved.state[[2]][[1]], list(.RNG.name = array("base::Su
                   append(saved.state[[2]][[3]], list(.RNG.name = array("base::Mersenne-Twister"), .RNG.seed = array(18))))
 
 # model
-jm <- jags.model(file = "models/cover-invasive/Invasive_cover_zoib.jags",
+jm <- jags.model(file = "models/cover-invasive/Invasive_cover_zib.jags",
                  inits = initslist,
                  n.chains = 3,
                  data = datlist)
@@ -129,9 +118,11 @@ dic.out
 gel <- gelman.diag(coda.out, multivariate = FALSE)
 gel
 
+# If not converged, restart model from final iterations
+# newinits <-  initfind(coda.out)
+# newinits[[1]]
+# saved.state <- removevars(newinits, variables = c(2, 4, 6:7))
+# saved.state[[1]]
+# save(saved.state, file = "models/cover-invasive/inits.Rdata")
 
-# Model fit
-params <- c("y.discrete.rep", "y.d.rep", "y.c.rep") #monitor replicated data
-coda.rep <- coda.samples(jm, variable.names = params,
-                         n.iter = 15000, thin = 5)
-
+save(coda.out, file = "models/cover-invasive/coda/coda.Rdata")
